6ab7553233f3b7b9cb839517bee7112bd148168e
tidus9000 <tidus9000@gmail.com>
2019-01-08 15:40:22 +0000

    started implementing flocking behaviour

--- CrowdSimulation/Assets/Scripts/Agent.cs
+++ CrowdSimulation/Assets/Scripts/Agent.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour {
 
-    [SerializeField] Vector2 m_velocity;
+    public Vector2 m_velocity;
     [SerializeField] float m_maxspeed;
     [SerializeField] float slowdown;
+    [SerializeField] float nearbyRadius = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,22 @@ public class Agent : MonoBehaviour {
         position += m_velocity * Time.deltaTime;
         transform.position = position;
 
+        //getting average direction of surrounding agents
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, nearbyRadius);
+        int i = 0;
+        Vector2 averageDirection = Vector2.zero;
+        foreach (Collider2D col in colliders)
+        {
+            if (col.CompareTag("Agent"))
+            {
+                GameObject agent = col.gameObject;
+                averageDirection += agent.GetComponent<Agent>().m_velocity;
+                i++;
+            }
+        }
+        averageDirection /= i;
+        m_velocity = averageDirection;
+
         m_velocity *= slowdown;
 
         if (m_velocity.magnitude <= 0.1)
@@ -27,6 +44,7 @@ public class Agent : MonoBehaviour {
         }
 
     }
+
 
     public void AddForce(Vector2 _force)
     {
