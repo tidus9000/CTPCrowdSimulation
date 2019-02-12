d22a3db0b0921c2183a143e3114f49d5e5445262
tidus9000 <tidus9000@gmail.com>
2019-02-12 09:20:39 +0000

    trying tom implement cohesion but also fixed a bug where agents were colliding with themselves

--- CrowdSimulation/Assets/Scripts/Agent.cs
+++ CrowdSimulation/Assets/Scripts/Agent.cs
@@ -9,6 +9,8 @@ public class Agent : MonoBehaviour {
     [SerializeField] float slowdown;
     [SerializeField] float nearbyRadius = 1;
 
+    [SerializeField]Vector2 averagePosition = Vector2.zero;
+
     bool m_forceAdded = false;
 
 	// Use this for initialization
@@ -21,21 +23,35 @@ public class Agent : MonoBehaviour {
 
         if (!m_forceAdded)
         {
+            bool nearby = false;
+
             //getting average direction of surrounding agents
             Collider2D[] colliders = Physics2D.OverlapCircleAll(position, nearbyRadius);
             int i = 0;
             Vector2 averageDirection = Vector2.zero;
             foreach (Collider2D col in colliders)
             {
-                if (col.CompareTag("Agent"))
+                if (col.CompareTag("Agent") && col.gameObject != this.gameObject)
                 {
                     GameObject agent = col.gameObject;
                     averageDirection += agent.GetComponent<Agent>().m_velocity;
+                    averagePosition.x += agent.transform.position.x;
+                    averagePosition.y += agent.transform.position.y;
                     i++;
+                    nearby = true;
                 }
             }
-            averageDirection /= i;
-            m_velocity = averageDirection;
+
+            if (nearby)
+            {
+                averageDirection /= i;
+                averagePosition /= i;
+                m_velocity = averageDirection;
+
+                //get average position of surrounding agents for cohesion
+                m_velocity += Vector2.MoveTowards(transform.position, averagePosition, .1f);
+            }
+
 
             m_velocity *= slowdown;
 
@@ -43,6 +59,7 @@ public class Agent : MonoBehaviour {
             {
                 m_velocity = Vector2.zero;
             }
+            averagePosition = Vector2.zero;
         }
 
 
@@ -60,6 +77,7 @@ public class Agent : MonoBehaviour {
         Vector3 normal = m_velocity;
         Gizmos.color = Color.white;
         Gizmos.DrawLine(transform.position, transform.position + normal);
+        UnityEditor.Handles.DrawWireDisc(averagePosition, Vector3.forward, 0.1f);
         UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, nearbyRadius);
     }
 
