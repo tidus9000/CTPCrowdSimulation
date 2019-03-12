19b9eb756fa246af37ede8832761600ea51d3418
tidus9000 <tidus9000@gmail.com>
2019-03-12 17:44:17 +0000

    made some changes to hopefully fix the grouping problem

--- CrowdSimulation/Assets/Scripts/Wall.cs
+++ CrowdSimulation/Assets/Scripts/Wall.cs
@@ -40,25 +40,25 @@ public class Wall : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //// force is how forcefully we will push the agent
-        //float force = 5;
+        // force is how forcefully we will push the agent
+        float force = 5;
 
-        //// If the object we hit is the enemy
-        //if (collision.gameObject.tag == "Agent")
-        //{
-        //    // Calculate Angle Between the collision point and the player
-        //    Vector2 dir = collision.contacts[0].point;
-        //    dir.x -= collision.transform.position.x;
-        //    dir.y -= collision.transform.position.y;
-        //    Vector2 heading = collision.contacts[0].point;
-        //    heading.x -= collision.gameObject.transform.position.x;
-        //    heading.y -= collision.gameObject.transform.position.y;
-        //    // We then get the opposite (-Vector3) and normalize it
-        //    heading = -heading.normalized;
-        //    // And finally we add force in the direction of dir and multiply it by force. 
-        //    // This will push back the agent
-        //    collision.gameObject.GetComponent<Agent>().AddForce(heading * force);
-        //}
+        // If the object we hit is the agent
+        if (collision.gameObject.tag == "Agent")
+        {
+            // Calculate Angle Between the collision point and the player
+            Vector2 dir = collision.contacts[0].point;
+            dir.x -= collision.transform.position.x;
+            dir.y -= collision.transform.position.y;
+            Vector2 heading = collision.contacts[0].point;
+            heading.x -= collision.gameObject.transform.position.x;
+            heading.y -= collision.gameObject.transform.position.y;
+            // We then get the opposite (-Vector3) and normalize it
+            heading = -heading.normalized;
+            // And finally we add force in the direction of dir and multiply it by force. 
+            // This will push back the agent
+            collision.gameObject.GetComponent<Agent>().AddForce(heading * force);
+        }
     }
 
 #if (UNITY_EDITOR)
